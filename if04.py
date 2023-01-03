d7af0300957371119ec9c545d1e35ad334efb12e
PRESTIGE <mamatqulov@gmail.com>
2023-01-03 17:31:05 +0500

    04

--- if04.py
+++ if04.py
@@ -1,4 +1,4 @@
-def main(a,b,c):
+def main(a, b, c):
     """
     Find how many positive numbers there are in the given numbers.
     Args:
@@ -8,4 +8,14 @@ def main(a,b,c):
     returns:
         integer: the number of positive numbers in the given numbers
     """
-    return
+    ans = 0
+    if a > 0:
+        ans += 1
+    if b > 0:
+        ans += 1
+    if b > 0:
+        ans += 1
+    return ans
+
+
+print(main(-2, 4, 3))
