c72ff3ac5d2fa770f31d09abfbc53ebba89c0af3
PRESTIGE <mamatqulov@gmail.com>
2023-01-03 18:22:48 +0500

    06

--- if06.py
+++ if06.py
@@ -1,4 +1,4 @@
-def main(a,b,c):
+def main(a, b, c):
     """
     Find how many positive and how many negative numbers there are in the given numbers.
     check the following conditions:
@@ -13,4 +13,24 @@ def main(a,b,c):
     Returns:
         string: string with the result
     """
-    return
+    p = 0
+    n = 0
+    if a > 0:
+        p += 1
+    else:
+        n += 1
+    if b > 0:
+        p += 1
+    else:
+        n += 1
+    if c > 0:
+        p += 1
+    else:
+        n += 1
+
+    if p > n:
+        return 'there a lot of positive numbers'
+    return 'there a lot of negative numbers'
+
+
+print(main(2, -1, -6))
